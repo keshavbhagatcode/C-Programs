8465e190f75a34cc5026b04945a32c6c833ffede
Keshav Bhagat <keshavbhagatdev1@gmail.com>
2026-09-22 19:43:38 +0530

    Variable Swap Using Bits

---
+++ VarSwapBit.c
@@ -0,0 +1,13 @@
+//Swapping values of two variables using bitwise XOR operator
+#include <stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("After swapping: a = %d, b = %d\n", a, b);
+}
